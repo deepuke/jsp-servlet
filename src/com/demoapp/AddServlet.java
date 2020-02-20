eeb1052af18942217aed5d9954e2d4743fa48388
Deepu Elayakunju <dipuke@gmail.com>
2020-02-20 07:24:44 +0530

    ServletContext and ServletConfig

--- src/com/demoapp/AddServlet.java
+++ src/com/demoapp/AddServlet.java
@@ -4,27 +4,38 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int a = Integer.parseInt(req.getParameter("num1"));
 		int b = Integer.parseInt(req.getParameter("num2"));
-		
-		int result = a+b;
-		
-		res.sendRedirect("sq?res="+result);
-		
-//		req.setAttribute("num", result);
-//		RequestDispatcher rd = req.getRequestDispatcher("sq");
-//		rd.forward(req, res);
-		
-//		PrintWriter out = res.getWriter();
-//		out.println("Result is "+result);
+
+		int result = a + b;
+		// Getting params from servlet context
+		ServletContext ctx = getServletContext();
+		String name = ctx.getInitParameter("Name");
+		System.out.println(name);
+
+		// Getting params from servlet config
+		ServletConfig scg = getServletConfig();
+		String cname = scg.getInitParameter(name);
+		System.out.println(cname);
+
+		res.sendRedirect("sq?res=" + result);
+
+		// req.setAttribute("num", result);
+		// RequestDispatcher rd = req.getRequestDispatcher("sq");
+		// rd.forward(req, res);
+
+		// PrintWriter out = res.getWriter();
+		// out.println("Result is "+result);
 	}
 
 }
